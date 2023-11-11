c173b1b3730e4d388cbe20f67ad822fc50ff4a55
Aaron Levy <aaronlevypc21@gmail.com>
2023-11-11 16:29:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName()  {
+    customerName = customerName.toLocaleUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Aaron'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not Aaron'
+}
